effbcf8cb78095778afdbab871268ed8824bb91d
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-16 21:50:24 +0800

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button 
+        @click="$emit('btn-click')"
+        class="btn"
+        :style="{background: bg_color}"
+        >
+        {{ text }}
+        <img :src="src" :alt="alt">
+    </button>
+</template>
+
+<script>
+
+    export default {
+        name: 'Button',
+        props: {
+            text: {
+                type: String,
+                default: 'This is a button'
+            },
+            bg_color: 'white',
+            src: '',
+            alt: '',
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
