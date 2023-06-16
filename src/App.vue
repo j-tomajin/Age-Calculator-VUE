30550946de75160110edef5236601685d74a1692
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-16 21:49:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import DateField from './components/DateField.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      DateField,
+    }
+  }
+
+</script>
+
+<template>
+  <main class="main">
+    <DateField />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use './assets/scss/utilities' as *;
+
+  .main {
+    padding: clampf(16, 780, 32);
+  }
+</style>
